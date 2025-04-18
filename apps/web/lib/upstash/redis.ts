be233257cd73d0b8564bc2255a17135d4ca001a1
Kishan Kumar <kishan.kumar@plivo.com>
2025-04-18 18:16:56 +0530

    feat: Switch from Upstash Redis to local ioredis

--- apps/web/lib/upstash/redis.ts
+++ apps/web/lib/upstash/redis.ts
@@ -1,7 +1,12 @@
-import { Redis } from "@upstash/redis";
+import Redis from "ioredis";
 
-// Initiate Redis instance by connecting to REST URL
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL || "",
-  token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
-});
+// Initiate Redis instance by connecting to localhost:6379
+export const redis = new Redis();
+
+// Optionally, you can use environment variables like this:
+// export const redis = new Redis({
+//   host: process.env.REDIS_HOST || '127.0.0.1',
+//   port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
+//   // Add password if your local redis requires it
+//   // password: process.env.REDIS_PASSWORD,
+// });
